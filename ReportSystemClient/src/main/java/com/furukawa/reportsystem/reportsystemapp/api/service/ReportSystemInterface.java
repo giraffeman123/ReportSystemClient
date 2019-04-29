9186657ad512983247c0e0fabeba097aca2596a3
giraffeman123 <36062624+giraffeman123@users.noreply.github.com>
2019-04-28 21:05:56 -0700

    Se validó el codigo de mantenimiento de lideres

--- ReportSystemClient/src/main/java/com/furukawa/reportsystem/reportsystemapp/api/service/ReportSystemInterface.java
+++ ReportSystemClient/src/main/java/com/furukawa/reportsystem/reportsystemapp/api/service/ReportSystemInterface.java
@@ -7,6 +7,7 @@ import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.DELETE;
 import retrofit2.http.Field;
 import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
--- ReportSystemClient/src/main/java/com/furukawa/reportsystem/reportsystemapp/ui/lider/ModificarLider.java
+++ ReportSystemClient/src/main/java/com/furukawa/reportsystem/reportsystemapp/ui/lider/ModificarLider.java
@@ -7,14 +7,19 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.furukawa.reportsystem.reportsystemapp.R;
+import com.furukawa.reportsystem.reportsystemapp.api.model.Empleado;
 import com.furukawa.reportsystem.reportsystemapp.api.model.Lider;
 import com.furukawa.reportsystem.reportsystemapp.api.service.ApiUtils;
 import com.furukawa.reportsystem.reportsystemapp.api.service.ReportSystemInterface;
+
+import java.util.ArrayList;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -25,84 +30,195 @@ import retrofit2.Response;
  * Activities that contain this fragment must implement the
  * {@link ModificarLider.OnFragmentInteractionListener} interface
  * to handle interaction events.
+ * Use the {@link ModificarLider#newInstance} factory method to
+ * create an instance of this fragment.
  */
 public class ModificarLider extends Fragment {
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String CODIGO_EMPLEADO = "codigo_empleado";
+    private static final String AREA = "area";
+    private static final String LINEA = "linea";
+    private static final String NOMBRE = "nombre";
+    private static final String PUESTO = "puesto";
+    private static final String TURNO = "turno";
 
+    // TODO: Rename and change types of parameters
     private Lider lider;
+    private ArrayAdapter<String> adapterLinea;
+    private ArrayAdapter<String> adapterTurno;
+    private ArrayAdapter<String> adapterArea;
     private ReportSystemInterface api;
-    private OnSendingLiderListener listener;
+
+    private OnFragmentInteractionListener mListener;
 
     public ModificarLider() {
         // Required empty public constructor
     }
 
-    public interface OnSendingLiderListener{
-        void onInputBuscarLiderSent(Lider lider);
+    /**
+     *
+     * @param codigo_empleado
+     * @param area
+     * @param linea
+     * @param nombre
+     * @param puesto
+     * @param turno
+     * @return
+     */
+    // TODO: Rename and change types and number of parameters
+    public static ModificarLider newInstance(String codigo_empleado, String area, String linea, String nombre,
+                                             String puesto, String turno) {
+        ModificarLider fragment = new ModificarLider();
+        Bundle args = new Bundle();
+        args.putString(CODIGO_EMPLEADO, codigo_empleado);
+        args.putString(AREA, area);
+        args.putString(LINEA, linea);
+        args.putString(NOMBRE, nombre);
+        args.putString(PUESTO, puesto);
+        args.putString(TURNO, turno);
+        fragment.setArguments(args);
+        return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            lider = new Lider(getArguments().getString(CODIGO_EMPLEADO),getArguments().getString(AREA),
+                    Integer.parseInt(getArguments().getString(LINEA)));
+            Empleado empleado = new Empleado(getArguments().getString(CODIGO_EMPLEADO),
+                    getArguments().getString(NOMBRE),getArguments().getString(PUESTO),
+                    getArguments().getString(TURNO));
+            lider.setEmpleado(empleado);
+        }
         api = ApiUtils.getReportSystemService();
+        initializeSpinners();
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_modificar_lider, container, false);
-        final EditText codigoEdTxt = v.findViewById(R.id.edtxtIngreseCodigo);
+        final EditText edtxtCodigo = v.findViewById(R.id.edtxtCodigoDeEmpleado);
+        final EditText edtxtNombre = v.findViewById(R.id.edtxtNombre);
+
+        final Spinner spLinea = v.findViewById(R.id.spLinea);
+        spLinea.setAdapter(adapterLinea);
+
+        final Spinner spArea = v.findViewById(R.id.spArea);
+        spArea.setAdapter(adapterArea);
+
+        final Spinner spTurno = v.findViewById(R.id.spTurno);
+        spTurno.setAdapter(adapterTurno);
+
         final Button btAceptar = v.findViewById(R.id.btAceptar);
+
+        edtxtCodigo.setText(lider.getCodigoEmpleado());
+        edtxtNombre.setText(lider.getEmpleado().getNombre());
+
+        int spPosLinea = adapterLinea.getPosition(String.valueOf(lider.getLinea()));
+        spLinea.setSelection(spPosLinea);
+
+        int spPosArea = adapterArea.getPosition(lider.getArea());
+        spArea.setSelection(spPosArea);
+
+        int spPosTurno = adapterTurno.getPosition(lider.getEmpleado().getTurno());
+        spTurno.setSelection(spPosTurno);
+
         btAceptar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                buscarLider(codigoEdTxt.getText().toString());
+                if(edtxtCodigo.getText().toString().isEmpty() || edtxtNombre.getText().toString().isEmpty()
+                        || spLinea.getSelectedItem().toString().equals("Seleccione una")
+                        || spArea.getSelectedItem().toString().equals("Seleccione una") ||
+                        spTurno.getSelectedItem().toString().equals("Seleccione una")) {
+                    Toast.makeText(getActivity(), "Hay un campo vacío. Antes de presionar agregar llene todos los campos."
+                            , Toast.LENGTH_LONG).show();
+                }else {
+                    modificarLider(edtxtCodigo.getText().toString(), spArea.getSelectedItem().toString(),
+                            spLinea.getSelectedItem().toString(), edtxtNombre.getText().toString(), "lider",
+                            spTurno.getSelectedItem().toString());
+                }
             }
         });
         return v;
     }
 
-    public void buscarLider(String codigoEmpleado){
-        if(!codigoEmpleado.isEmpty()){
-            Call<Lider> call = api.getLiderByCodigoEmpleado(codigoEmpleado);
-            call.enqueue(new Callback<Lider>() {
-                @Override
-                public void onResponse(Call<Lider> call, Response<Lider> response) {
-                    if(!response.isSuccessful()){
-                        Toast.makeText(getActivity(),"Code: "+response.code(),Toast.LENGTH_LONG).show();
-                    }
-
-                    if(response.body() != null){
-                        lider = response.body();
-                        listener.onInputBuscarLiderSent(lider);
-                        Toast.makeText(getActivity(),"Lider Encontrado",Toast.LENGTH_LONG).show();
-                    }else
-                        Toast.makeText(getActivity(),"Lider no encontrado",Toast.LENGTH_LONG).show();
+    public void modificarLider(String codigo_empleado, String area, String linea, String nombre,
+                               String puesto, String turno){
+        Call<String> call = api.updateLider(codigo_empleado,area,linea,nombre,puesto,turno);
+        call.enqueue(new Callback<String>() {
+            @Override
+            public void onResponse(Call<String> call, Response<String> response) {
+                if(!response.isSuccessful()){
+                    Toast.makeText(getActivity(),"Code: "+response.code(),Toast.LENGTH_LONG).show();
                 }
-
-                @Override
-                public void onFailure(Call<Lider> call, Throwable t) {
-                    Toast.makeText(getActivity(),t.getMessage(),Toast.LENGTH_LONG).show();
+                if(response.body() == null){
+                    Toast.makeText(getActivity(),"Hubo un error en la modificación",Toast.LENGTH_LONG).show();
+                }else{
+                    Toast.makeText(getActivity(),"Lider Modificado", Toast.LENGTH_LONG).show();
                 }
-            });
-        }else
-            Toast.makeText(getActivity(),"Ingrese un codigo a buscar",Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onFailure(Call<String> call, Throwable t) {
+                Toast.makeText(getActivity(),t.getMessage(),Toast.LENGTH_LONG).show();
+            }
+        });
     }
 
+    public void initializeSpinners(){
+        ArrayList<String> arraySpinnerLinea = new ArrayList<>();
+        arraySpinnerLinea.add("Seleccione una");
+        arraySpinnerLinea.add("1");
+        arraySpinnerLinea.add("2");
+        arraySpinnerLinea.add("3");
+        arraySpinnerLinea.add("4");
+        arraySpinnerLinea.add("5");
+        arraySpinnerLinea.add("6");
+        arraySpinnerLinea.add("7");
+
+        ArrayList<String> arraySpinnerArea = new ArrayList<>();
+        arraySpinnerArea.add("Seleccione una");
+        arraySpinnerArea.add("Fusebox");
+        arraySpinnerArea.add("SRC");
+        arraySpinnerArea.add("Ensamblaje");
+        arraySpinnerArea.add("Produccion");
+
+        ArrayList<String> arraySpinnerTurno = new ArrayList<>();
+        arraySpinnerTurno.add("Seleccione una");
+        arraySpinnerTurno.add("Matutino");
+        arraySpinnerTurno.add("Intermedio");
+        arraySpinnerTurno.add("Vespertino");
+
+        adapterLinea = new ArrayAdapter<String>(this.getActivity().getApplicationContext(),android.R.layout.simple_spinner_dropdown_item,arraySpinnerLinea);
+        adapterArea = new ArrayAdapter<String>(this.getActivity().getApplicationContext(),android.R.layout.simple_spinner_dropdown_item,arraySpinnerArea);
+        adapterTurno = new ArrayAdapter<String>(this.getActivity().getApplicationContext(),android.R.layout.simple_spinner_dropdown_item,arraySpinnerTurno);
+    }
+
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
 
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
-        if(context instanceof OnSendingLiderListener){
-            listener = (OnSendingLiderListener) context;
-        }else {
-            throw new RuntimeException(context.toString()+" must implement OnSendingLiderListener");
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
         }
     }
 
     @Override
     public void onDetach() {
         super.onDetach();
-        listener = null;
+        mListener = null;
     }
 
     /**
